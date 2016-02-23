6b09e1c2b4419fe9a11f1c77906420ae2d43c476
mgreenegit <migreene@outlook.com>
2016-02-23 12:02:18 -0600

    integrate BeforeAll and AfterAll to restore setting on persistent build nodes

--- Tests/PowerPlan.Tests.ps1
+++ Tests/PowerPlan.Tests.ps1
@@ -1,46 +1,52 @@
 #Requires -modules PowerPlan -RunAsAdministrator
 
 Describe 'Get information from localhost about power plans' -tag 'taskRunnertest' {
-    $builtIn = @('Balanced', 'High performance', 'Power saver')
-    $plans = Get-Powerplan
-    Context 'List power plans' {
-        ForEach ($plan in $builtIn)
-        {
-            It "the returned list includes the $plan plan" {
-                $plans.ElementName -contains  $plan | Should Be True
+    try {
+        BeforeAll {
+            $currentPlan = $plans | Where-Object {$_.IsActive -eq $true}
+        }
+        $builtIn = @('Balanced', 'High performance', 'Power saver')
+        $plans = Get-Powerplan
+        Context 'List power plans' {
+            ForEach ($plan in $builtIn)
+            {
+                It "the returned list includes the $plan plan" {
+                    $plans.ElementName -contains  $plan | Should Be True
+                }
+                It "can specifically get the $plan plan" {
+                    (Get-Powerplan -PlanName $plan).ElementName | Should Be $plan
+                }
             }
-            It "can specifically get the $plan plan" {
-                (Get-Powerplan -PlanName $plan).ElementName | Should Be $plan
+            It 'uses the -ComputerName parameter correctly against localhost' {
+                $ComputerName = 'localhost'
+                $results = Get-Powerplan -ComputerName $ComputerName
+                $results.count -gt 0 | Should Be True
+            }
+            It 'returns the expected properties of a plan' {
+                $properties = Get-Powerplan | Get-Member -MemberType Property
+                $properties.Name | Should Be @('Caption', 'Description', 'ElementName', 'InstanceID', 'IsActive', 'PSComputerName')
             }
         }
-        It 'uses the -ComputerName parameter correctly against localhost' {
-            $ComputerName = 'localhost'
-            $results = Get-Powerplan -ComputerName $ComputerName
-            $results.count -gt 0 | Should Be True
-            # would be better to count errors
-        }
-        It 'returns the expected properties of a plan' {
-            $properties = Get-Powerplan | Get-Member -MemberType Property
-            $properties.Name | Should Be @('Caption', 'Description', 'ElementName', 'InstanceID', 'IsActive', 'PSComputerName')
-        }
-    }
-    Context 'Set a Powerplan active' {
-        $currentPlan = $plans | Where-Object {$_.IsActive -eq $true}
-        ForEach ($plan in $builtIn)
-        {
-            It "sets $plan as active" {
-                Set-PowerPlan $plan
+        Context 'Set a Powerplan active' {
+            ForEach ($plan in $builtIn)
+            {
+                It "sets $plan as active" {
+                    Set-PowerPlan $plan
+                    $active = Get-PowerPlan | Where-Object {$_.IsActive -eq $true}
+                    $active.ElementName | Should Be $plan
+                }
+            }
+            It 'uses the -ComputerName parameter correctly against localhost' {
+                $ComputerName = 'localhost'
+                Set-Powerplan -ComputerName $ComputerName -PlanName 'Balanced'
                 $active = Get-PowerPlan | Where-Object {$_.IsActive -eq $true}
-                $active.ElementName | Should Be $plan
+                $active.ElementName | Should Be 'Balanced'
             }
         }
-        It 'uses the -ComputerName parameter correctly against localhost' {
-            $ComputerName = 'localhost'
-            Set-Powerplan -ComputerName $ComputerName -PlanName 'Balanced'
-            $active = Get-PowerPlan | Where-Object {$_.IsActive -eq $true}
-            $active.ElementName | Should Be 'Balanced'
-            # would be better to count errors
         }
-        Set-PowerPlan $currentPlan.ElementName
+    finally {
+        AfterAll {
+            Set-PowerPlan $currentPlan.ElementName
+        }
     }
 }

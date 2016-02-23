6834919725a8141ce8dcd6fc6df1f14ff9addc14
mgreenegit <migreene@outlook.com>
2016-02-23 09:13:38 -0600

    Add test to validate default plans are returned

--- Tests/PowerPlan.Tests.ps1
+++ Tests/PowerPlan.Tests.ps1
@@ -1,12 +1,14 @@
 #Requires -modules PowerPlan -RunAsAdministrator
 
-Describe 'Get information from localhost about power plans'
-{
-    Context 'List all power plans'
-    {
-        It 'list of plan names'
+Describe 'Get information from localhost about power plans' -tag 'taskRunnertest' {
+    Context 'List power plans' {
+        $plans = Get-PowerPlan
+        $builtIn = @('Balanced', 'High performance', 'Power saver')
+        ForEach ($plan in $builtIn)
         {
-            Get-PowerPlan | ForEach-Object 'ElementName' | Should Include @('Balanced', 'HIgh performance', 'Power saver')
+            It "includes the $plan plan" {
+                ($plans | ForEach-Object 'ElementName') -contains  $plan | Should Be True
+            }
         }
     }
 }

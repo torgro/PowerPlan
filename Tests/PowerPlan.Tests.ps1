0c8a9f2a062fb2489c719b726c23515ead2052c0
mgreenegit <migreene@outlook.com>
2016-02-23 09:17:34 -0600

    validate parameter -planname

--- Tests/PowerPlan.Tests.ps1
+++ Tests/PowerPlan.Tests.ps1
@@ -2,12 +2,14 @@
 
 Describe 'Get information from localhost about power plans' -tag 'taskRunnertest' {
     Context 'List power plans' {
-        $plans = Get-PowerPlan
         $builtIn = @('Balanced', 'High performance', 'Power saver')
         ForEach ($plan in $builtIn)
         {
-            It "includes the $plan plan" {
-                ($plans | ForEach-Object 'ElementName') -contains  $plan | Should Be True
+            It "the returned list includes the $plan plan" {
+                (Get-Powerplan | ForEach-Object 'ElementName') -contains  $plan | Should Be True
+            }
+            It "can specifically get the $plan plan" {
+                Get-Powerplan -PlanName $plan | ForEach-Object 'ElementName' | Should Be $plan
             }
         }
     }

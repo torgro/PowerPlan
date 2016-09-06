146e72aaf078acac3a21b06c1f0be3d7ec288248
Michael Greene <migreene@outlook.com>
2016-09-06 13:18:00 -0500

    Update tests to use Mocks

--- Tests/PowerPlan.Tests.ps1
+++ Tests/PowerPlan.Tests.ps1
@@ -1,63 +1,69 @@
 #Requires -modules PowerPlan -RunAsAdministrator
 
-Describe 'Get information from localhost about power plans' -tag 'taskRunnertest' {
-    try {
-        
-        BeforeAll
+Describe 'Discovers and and sets power plan' {
+    
+    $plans = Get-Powerplan
+    
+    Context 'List power plans' {
+        ForEach ($plan in $plans)
         {
-            $restorePlan = Get-CimInstance -Namespace 'root\cimv2\power' -ClassName 'Win32_PowerPlan' -Filter 'IsActive=True'
-        }
-        
-        $builtIn = @('Balanced', 'High performance', 'Power saver')
-        $plans = Get-Powerplan
-        
-        Context 'List power plans' {
-            ForEach ($plan in $builtIn)
-            {
-                It "the returned list includes the $plan plan" {
-                    $plans.ElementName -contains  $plan | Should Be True
-                }
-                It "can specifically get the $plan plan" {
-                    (Get-Powerplan -PlanName $plan).ElementName | Should Be $plan
-                }
+            $plan = $plan.ElementName
+
+            It "the returned list includes the $plan plan" {
+                $plans.ElementName -contains  $plan | Should Be True
             }
-            It 'uses the -ComputerName parameter correctly against localhost' {
-                $ComputerName = 'localhost'
-                $results = Get-Powerplan -ComputerName $ComputerName
-                $results.count -gt 0 | Should Be True
-            }
-            It 'returns the expected properties of a plan' {
-                $properties = Get-Powerplan | Get-Member -MemberType Property
-                $properties.Name | Should Be @('Caption', 'Description', 'ElementName', 'InstanceID', 'IsActive', 'PSComputerName')
-            }
-            It 'returns the expected plan using the -Active parameter' {
-                $active = Get-CimInstance -Namespace 'root\cimv2\power' -ClassName 'Win32_PowerPlan' -Filter 'IsActive=True'
-                (Get-Powerplan -Active).InstanceID | Should Be $active.InstanceID
+            It "can specifically get the $plan plan" {
+                (Get-Powerplan -PlanName $plan).ElementName | Should Be $plan
             }
         }
-        
-        Context 'Set a Powerplan active' {
-            ForEach ($plan in $builtIn)
-            {
-                It "sets $plan as active" {
-                    Set-PowerPlan $plan
-                    $active = Get-CimInstance -Namespace 'root\cimv2\power' -ClassName 'Win32_PowerPlan' -Filter 'IsActive=True'
-                    $active.ElementName | Should Be $plan
-                }
-            }
-            It 'uses the -ComputerName parameter correctly against localhost' {
-                $ComputerName = 'localhost'
-                Set-Powerplan -ComputerName $ComputerName -PlanName 'Balanced'
-                $active = Get-CimInstance -Namespace 'root\cimv2\power' -ClassName 'Win32_PowerPlan' -Filter 'IsActive=True'
-                $active.ElementName | Should Be 'Balanced'
-            }
+        It 'uses the -ComputerName parameter correctly against localhost' {
+            $ComputerName = 'localhost'
+            $results = Get-Powerplan -ComputerName $ComputerName | ForEach-Object ElementName
+            $results.count -gt 0 | Should Be True
+        }
+        It 'returns the expected properties of a plan' {
+            $properties = Get-Powerplan | Get-Member -MemberType Property
+            $properties.Name | Should Be @('Caption', 'Description', 'ElementName', 'InstanceID', 'IsActive', 'PSComputerName')
+        }
+        It 'returns the expected plan using the -Active parameter' {
+            $active = Get-CimInstance -Namespace 'root\cimv2\power' -ClassName 'Win32_PowerPlan' -Filter 'IsActive=True'
+            (Get-Powerplan -Active).InstanceID | Should Be $active.InstanceID
         }
     }
-    finally {
-        AfterAll
+    
+    Context 'Set a Powerplan active' {
+
+        Mock -ModuleName PowerPlan -CommandName Invoke-CimMethod -MockWith {}
+
+        ForEach ($plan in $plans)
         {
-            $inputObject = Get-CimInstance -Namespace 'root\cimv2\power' -ClassName 'Win32_PowerPlan' | Where-Object {$_.ElementName -eq $restorePlan.ElementName}
-            $null = Invoke-CimMethod -InputObject $inputObject -MethodName Activate
+            $plan = $plan.ElementName
+
+            It "handles the plan $plan without error" {
+                {Set-PowerPlan $plan} | Should Not Throw
+            }
+            It "calls the correct mocks" {
+                Assert-MockCalled -ModuleName PowerPlan -CommandName Invoke-CimMethod -Times 1
+            }
+        }
+        It 'uses the -ComputerName parameter correctly against localhost' {
+            $ComputerName = 'localhost'
+            {Set-Powerplan -ComputerName $ComputerName -PlanName 'Balanced'} | Should Not Throw
+        }
+        It "calls the correct mocks" {
+            Assert-MockCalled -ModuleName PowerPlan -CommandName Invoke-CimMethod -Times 1
+        }
+    }
+
+    Context 'Set-PowerPlan implements -WhatIf' {
+
+        Mock -ModuleName PowerPlan -CommandName Invoke-CimMethod -MockWith {}
+
+        It "handles -whatif without error" {
+            {Set-PowerPlan 'Balanced' -whatif} | Should Not Throw
+        }
+        It "does not call the mock" {
+            Assert-MockCalled -ModuleName PowerPlan -CommandName Invoke-CimMethod -Times 0
         }
     }
 }
